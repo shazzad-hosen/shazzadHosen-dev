6188333769cab7f5ea849c3e518a9a613a1f9d57
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-05-01 12:45:44 +0600

    update vite api url to backend url

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -8,7 +8,7 @@ router.post("/", async (req, res) => {
     const { name, email, message } = req.body;
 
     if (!name || !email || !message) {
-      return res.status(400).json({ message: "All fields are required" });
+      return res.status(400).json({ message: "All fields are required " });
     }
 
     await sendEmail({ name, email, message });
